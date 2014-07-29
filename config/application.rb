60bdab0baa23d2bf6d2cf335391a2acc3f057b17
Lennon Shimokawa <lshimokawa@gmail.com>
2014-07-29 12:59:32 -0500

    Configurar assets para Heroku

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RailsComicBookstore
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    config.serve_static_assets = true
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
